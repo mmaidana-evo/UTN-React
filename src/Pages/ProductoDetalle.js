c98a5a0a22a79bac044c412b465ecfcfdc8eaede
maurdev <maur.develop@gmail.com>
2020-12-27 20:43:39 -0300

    se agrega visualizador de loading

--- src/Pages/ProductoDetalle.js
+++ src/Pages/ProductoDetalle.js
@@ -1,8 +1,10 @@
 import React,{useState,useEffect} from "react";
 import {getProducto} from "../Services/ServiceProductos";
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 function ProductoDetalle(props){    
+    const [loading,setLoading] = useState(true);
     const [producto,setProducto] = useState({})
 
     useEffect(
@@ -10,21 +12,32 @@ function ProductoDetalle(props){
             getProducto(props.match.params.id)
             .then((data)=>{
                 // console.log('Resp. getProducto: ',data.data[0]);
+                setLoading(false);
                 setProducto(data.data[0])
             })
         },[]
     )
    
-    return(
-        <>
-        <legend><span class="balloon">!</span> {producto.name}</legend>
-        <main>
-            <p>{producto.description}</p>
-            <div style={{textAlign:"right"}}>Precio: $ {producto.price}</div>
-            <Button variant="primary">Comprar</Button>
-        </main>
-        </>
-    )
+    if(loading){
+        return(
+            <div style={{textAlign:"center"}}>
+                <Spinner animation="border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </Spinner>
+            </div>
+        )
+    }else{        
+        return(
+            <>
+            <legend><span class="balloon">!</span> {producto.name}</legend>
+            <main>
+                <p>{producto.description}</p>
+                <div style={{textAlign:"right"}}>Precio: $ {producto.price}</div>
+                <Button variant="primary">Comprar</Button>
+            </main>
+            </>
+        )
+    }
 }
 
 export default ProductoDetalle;
